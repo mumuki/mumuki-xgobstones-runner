3ade451daf8d97192e00166f7bd09c3424bb4a21
Federico Aloi <federico.aloi@gmail.com>
2015-05-05 01:01:55 -0300

    On success, the output is a html representation of the board

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -5,7 +5,16 @@ class TestRunner
     @config['gobstones_command']
   end
 
+  def post_process_file(file, result, status)
+    begin
+      [status == :passed ? @output_file.read : result, status]
+    ensure
+      @output_file.close(true)
+    end
+  end
+
   def run_test_command(file)
-    "#{gobstones_path} #{file.path} --size 4 4"
+    @output_file = Tempfile.new(%w(gobstones .html))
+    "#{gobstones_path} #{file.path} --size 4 4 --to #{@output_file.path}"
   end
 end
--- spec/test_runner_spec.rb
+++ spec/test_runner_spec.rb
@@ -2,12 +2,18 @@ require_relative '../lib/test_runner'
 
 describe TestRunner do
   let(:runner) { TestRunner.new('gobstones_command' => 'python .heroku/vendor/pygobstones/language/vgbs/gbs.py') }
-  let(:file) { File.new('spec/data/example.gbs') }
 
   describe '#run_test_file' do
-    describe 'should pass when the file is sintactically ok' do
-      let(:results) { runner.run_test_file!(File.new('spec/data/example.gbs')) }  
-      it { expect(results[1]).to eq(:passed) }
+    describe 'when the file is sintactically ok,' do
+      let(:results) { runner.run_test_file!(File.new('spec/data/red_ball_at_origin.gbs')) }  
+      
+      it 'should output the board as html' do
+        expect(results[0]).to include(File.new('spec/data/red_ball_at_origin.html').read) 
+      end
+
+      it 'should pass the test' do
+        expect(results[1]).to eq(:passed) 
+      end
     end
 
     describe 'should fail when the file is not sintactically ok' do
