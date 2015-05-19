1faafdffb4cb1203a5ea15c1dd7bee884c182e19
Federico Aloi <federico.aloi@gmail.com>
2015-05-18 21:38:37 -0300

    A Board can tell if it is equal to another

--- lib/gobstones/board.rb
+++ lib/gobstones/board.rb
@@ -4,6 +4,8 @@ class OutOfBoundsError < RuntimeError
 end
 
 class Board
+  attr_reader :size, :cells
+
   def initialize(x, y)
     @size = [x, y]
     @cells = Hash.new(Cell.new)
@@ -21,5 +23,16 @@ class Board
 
   def contains(position)
     @size[0] > position.x && @size[1] > position.y
+  end
+
+  def ==(other)
+    self.class === other and
+        other.size == @size and
+        all_cells_equal(other)
+  end
+
+  def all_cells_equal(other)
+    @cells.all? { |position, cell| other.cell_at(position) == cell } and
+        other.cells.all? { |position, cell| cell_at(position) == cell }
   end
 end
--- spec/gobstones/board_spec.rb
+++ spec/gobstones/board_spec.rb
@@ -12,4 +12,33 @@ describe Board do
     it { expect(board.cell_at(Position.new(0, 1))).to eq(Cell.new) }
     it { lambda { board.cell_at(Position.new(4, 4)) }.should raise_error(OutOfBoundsError) }
   end
+
+  context 'should be able to compare itself with another board' do
+    let(:board) {
+      Board.new(4, 4)
+        .add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
+        .add_cell(Position.new(0, 1), Cell.new(green=4))
+    }
+
+    it { expect(board).not_to eq(Board.new(4, 4)) }
+
+    it {
+      another = Board.new(4, 4).add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
+      expect(board).not_to eq(another)
+    }
+
+    it {
+      another = Board.new(4, 4).add_cell(Position.new(0, 1), Cell.new(green=4)).add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
+      expect(board).to eq(another)
+    }
+
+    it {
+      another = Board.new(4, 4)
+        .add_cell(Position.new(0, 1), Cell.new(green=4))
+        .add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
+        .add_cell(Position.new(1, 0), Cell.new(black=7))
+
+      expect(board).not_to eq(another)
+    }
+  end
 end
