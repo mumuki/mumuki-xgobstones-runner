2454dc9080156d5a01502f7a3fbb98474892f5a4
Federico Aloi <federico.aloi@gmail.com>
2015-05-20 00:21:02 -0300

    The stones of a Cell can be set by name

--- spec/gobstones/board_spec.rb
+++ spec/gobstones/board_spec.rb
@@ -6,9 +6,9 @@ require_relative '../../lib/gobstones/cell'
 
 describe Board do
   context 'should store cells' do
-    let(:board) { Board.new(4, 4).add_cell(Position.new(0, 0), Cell.new(blue=3)) }
+    let(:board) { Board.new(4, 4).add_cell(Position.new(0, 0), Cell.new(blue: 3)) }
 
-    it { expect(board.cell_at(Position.new(0, 0))).to eq(Cell.new(blue=3)) }
+    it { expect(board.cell_at(Position.new(0, 0))).to eq(Cell.new(blue: 3)) }
     it { expect(board.cell_at(Position.new(0, 1))).to eq(Cell.new) }
     it { lambda { board.cell_at(Position.new(4, 4)) }.should raise_error(OutOfBoardError) }
   end
@@ -23,27 +23,27 @@ describe Board do
   context 'should be able to compare itself with another board' do
     let(:board) {
       Board.new(4, 4)
-        .add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
-        .add_cell(Position.new(0, 1), Cell.new(green=4))
+        .add_cell(Position.new(0, 0), Cell.new(blue: 3, red: 8))
+        .add_cell(Position.new(0, 1), Cell.new(green: 4))
     }
 
     it { expect(board).not_to eq(Board.new(4, 4)) }
 
     it {
-      another = Board.new(4, 4).add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
+      another = Board.new(4, 4).add_cell(Position.new(0, 0), Cell.new(blue: 3, red: 8))
       expect(board).not_to eq(another)
     }
 
     it {
-      another = Board.new(4, 4).add_cell(Position.new(0, 1), Cell.new(green=4)).add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
+      another = Board.new(4, 4).add_cell(Position.new(0, 1), Cell.new(green: 4)).add_cell(Position.new(0, 0), Cell.new(blue: 3, red: 8))
       expect(board).to eq(another)
     }
 
     it {
       another = Board.new(4, 4)
-        .add_cell(Position.new(0, 1), Cell.new(green=4))
-        .add_cell(Position.new(0, 0), Cell.new(blue=3, red=8))
-        .add_cell(Position.new(1, 0), Cell.new(black=7))
+        .add_cell(Position.new(0, 1), Cell.new(green: 4))
+        .add_cell(Position.new(0, 0), Cell.new(blue: 3, red: 8))
+        .add_cell(Position.new(1, 0), Cell.new(black: 7))
 
       expect(board).not_to eq(another)
     }
