420b3d0b90bea2af9d92e98376b8f201887d8ae0
Federico Aloi <federico.aloi@gmail.com>
2015-05-20 00:21:41 -0300

    The parser creates all the cells

--- spec/gobstones/gbb_parser_spec.rb
+++ spec/gobstones/gbb_parser_spec.rb
@@ -14,7 +14,7 @@ cell 1 0 Negro 3 Rojo 0 Verde 0 Azul 0
 head 3 3'
   }
 
-  let (:board) { parser.parseString gbb }
+  let (:board) { parser.from_string gbb }
 
   context 'should create a board with the proper size' do
     it { expect(board.size).to eq [4, 4] }
@@ -22,5 +22,10 @@ head 3 3'
 
   context 'should set the position of the head' do
     it { expect(board.head_position).to eq Position.new(3, 3) }
+  end
+
+  context 'should set the cells' do
+    it { expect(board.cell_at(Position.new 0, 0)).to eq Cell.new(red: 1, green: 2, blue: 5) }
+    it { expect(board.cell_at(Position.new 0, 1)).to eq Cell.new(black: 3) }
   end
 end
