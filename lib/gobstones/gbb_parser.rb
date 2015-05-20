68760671444f26b4e7a9509b34fb90c494c39674
Federico Aloi <federico.aloi@gmail.com>
2015-05-20 00:21:41 -0300

    The parser creates all the cells

--- lib/gobstones/gbb_parser.rb
+++ lib/gobstones/gbb_parser.rb
@@ -1,14 +1,63 @@
 require_relative 'board'
+require_relative 'cell'
+
+class MatchData
+  def to_position
+    Position.new self[1].to_i, self[2].to_i
+  end
+end
+
+class Array
+  def init(n=1)
+    take size - n
+  end
+end
 
 class GbbParser
-  def parseString(gbb_string)
+  def from_string(gbb_string)
     board = nil
 
     lines = gbb_string.lines
 
-    /size (\d+) (\d+)/.match(lines[1]) { |data| board = Board.new(data[1].to_i, data[2].to_i) }
-    /head (\d+) (\d+)/.match(lines.last) { |data| board.move_head_to Position.new(data[1].to_i, data[2].to_i) }
+    /size (\d+) (\d+)/.match(lines[1]) { |match| board = Board.new(match[1].to_i, match[2].to_i) }
+    /head (\d+) (\d+)/.match(lines.last) { |match| board.move_head_to match.to_position }
+
+    lines.drop(2).init.each { |cell_line|
+      position = get_position_from cell_line
+      cell = create_cell_from cell_line
+
+      board.add_cell position, cell
+    }
 
     board
+  end
+
+  private
+
+  def get_position_from(cell_line)
+    /cell (\d+) (\d+)/.match(cell_line).to_position
+  end
+
+  def create_cell_from(cell_line)
+    cell = Cell.new
+
+    cell_line.scan(/(Azul|Negro|Rojo|Verde) (\d+)/) { |match|
+      cell.set to_color(match[0]), match[1].to_i
+    }
+
+    cell
+  end
+
+  def to_color(gbb_color_string)
+    case gbb_color_string
+      when 'Azul'
+        :blue
+      when 'Negro'
+        :black
+      when 'Rojo'
+        :red
+      when 'Verde'
+        :green
+    end
   end
 end
