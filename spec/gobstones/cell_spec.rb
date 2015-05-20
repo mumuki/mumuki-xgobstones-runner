31d4159b343edb884fe3ca7ea245d65edfef8097
Federico Aloi <federico.aloi@gmail.com>
2015-05-20 00:51:44 -0300

    Moved setup code from an it to a before

--- spec/gobstones/cell_spec.rb
+++ spec/gobstones/cell_spec.rb
@@ -3,10 +3,7 @@ require 'rspec'
 describe Cell do
   context 'the stones can be set by color name' do
     let (:cell) { Cell.new }
-
-    it {
-      cell.set :red, 8
-      expect(cell.red). to eq 8
-    }
+    before { cell.set :red, 8 }
+    it { expect(cell.red). to eq 8 }
   end
 end
