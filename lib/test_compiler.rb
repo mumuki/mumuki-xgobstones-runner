79c599eda01dbde7431bae0151e187406745c683
Federico Aloi <federico.aloi@gmail.com>
2015-05-20 02:12:09 -0300

    The compiler gets the initial and final board from the test

--- lib/test_compiler.rb
+++ lib/test_compiler.rb
@@ -3,9 +3,12 @@ require 'yaml'
 
 class TestCompiler
   def compile(test_src, extra_src, content_src)
+    test = YAML::load(test_src)
+
     {
       :source => "#{content_src}\n#{extra_src}",
-      :initial_board => test_src
+      :initial_board => test['initial_board'],
+      :final_board => test['final_board'],
     }.to_yaml
   end
 end
