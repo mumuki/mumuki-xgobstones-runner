a3b919573dd7823b45b2f7482690a3412a7697cf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-21 15:58:12 -0300

    Wrapping gobstones code with Gobstones module

--- lib/gobstones/board.rb
+++ lib/gobstones/board.rb
@@ -1,51 +1,52 @@
-include Gobstones
+module Gobstones
 
-class Gobstones::OutOfBoardError < RuntimeError
-end
-
-class Gobstones::Board
-  attr_reader :size, :cells, :head_position
-
-  def initialize(x, y)
-    @size = [x, y]
-    @cells = Hash.new(Cell.new)
-    @head_position = Position.new(0, 0)
+  class OutOfBoardError < RuntimeError
   end
 
-  def add_cell(position, cell)
-    @cells[position] = cell
-    self
-  end
+  class Board
+    attr_reader :size, :cells, :head_position
 
-  def move_head_to(position)
-    check_is_within_bounds position
-    @head_position = position
-    self
-  end
+    def initialize(x, y)
+      @size = [x, y]
+      @cells = Hash.new(Cell.new)
+      @head_position = Position.new(0, 0)
+    end
 
-  def cell_at(position)
-    check_is_within_bounds position
-    @cells[position]
-  end
+    def add_cell(position, cell)
+      @cells[position] = cell
+      self
+    end
 
-  def ==(other)
-    self.class === other &&
-        other.size == @size &&
-        all_cells_equal?(other)
-  end
+    def move_head_to(position)
+      check_is_within_bounds position
+      @head_position = position
+      self
+    end
 
-  private
+    def cell_at(position)
+      check_is_within_bounds position
+      @cells[position]
+    end
 
-  def within_bounds?(position)
-    @size[0] > position.x && @size[1] > position.y
-  end
+    def ==(other)
+      self.class === other &&
+          other.size == @size &&
+          all_cells_equal?(other)
+    end
 
-  def check_is_within_bounds(position)
-    raise OutOfBoardError unless within_bounds?(position)
-  end
+    private
 
-  def all_cells_equal?(other)
-    @cells.all? { |position, cell| other.cell_at(position) == cell } &&
-        other.cells.all? { |position, cell| cell_at(position) == cell }
+    def within_bounds?(position)
+      @size[0] > position.x && @size[1] > position.y
+    end
+
+    def check_is_within_bounds(position)
+      raise OutOfBoardError unless within_bounds?(position)
+    end
+
+    def all_cells_equal?(other)
+      @cells.all? { |position, cell| other.cell_at(position) == cell } &&
+          other.cells.all? { |position, cell| cell_at(position) == cell }
+    end
   end
 end
