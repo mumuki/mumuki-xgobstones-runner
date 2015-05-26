5adde80ef5a888610dc464494f7498302a8d10aa
Federico Aloi <federico.aloi@gmail.com>
2015-05-26 20:09:28 -0300

    The compiler parser the check_head_position flag

--- spec/test_compiler_spec.rb
+++ spec/test_compiler_spec.rb
@@ -8,11 +8,18 @@ describe TestCompiler do
     let(:compiler) { TestCompiler.new }
     let(:test_file) {
 '
+check_head_position: true
+
 examples:
  - initial_board: initial
    final_board: final'
     }
     let(:output) { compiler.create_compilation!(test_file, 'extra', 'content') }
-    it { expect(output).to eq({source: "content\nextra", examples: [{initial_board: 'initial', final_board: 'final'}] }) }
+    it {
+      expect(output).to eq({
+        source: "content\nextra",
+        examples: [{initial_board: 'initial', final_board: 'final'}],
+        check_head_position: true })
+    }
   end
 end
