175f068e2f7afd01bcd7c05b63b67f0e97de2810
Federico Aloi <federico.aloi@gmail.com>
2015-05-28 15:34:18 -0300

    ExpectationsRunner uses the given Gobstones command

--- lib/gobstones/expectations/expectations_runner.rb
+++ lib/gobstones/expectations/expectations_runner.rb
@@ -4,8 +4,8 @@ module Gobstones::Expectations
   class ExpectationsRunner
     include Gobstones::WithTempfile
 
-    def initialize(gobstones_path, source_code)
-      @gobstones_path = gobstones_path
+    def initialize(gobstones_command, source_code)
+      @gobstones_command = gobstones_command
       @source_code = source_code
     end
 
@@ -17,7 +17,7 @@ module Gobstones::Expectations
     private
 
     def generate_ast!
-      %x"gbs #{write_tempfile(@source_code, 'gbs').path} --print-ast --no-print-board --no-print-retvals"
+      %x"#{@gobstones_command} #{write_tempfile(@source_code, 'gbs').path} --print-ast --no-print-board --no-print-retvals"
     end
   end
 end
