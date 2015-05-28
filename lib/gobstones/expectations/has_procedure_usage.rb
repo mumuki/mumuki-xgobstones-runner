00caf432dc88af8377d88e6eb07583080df376b6
Federico Aloi <federico.aloi@gmail.com>
2015-05-28 14:44:55 -0300

    The value actually checks the AST

--- lib/gobstones/expectations/has_procedure_usage.rb
+++ lib/gobstones/expectations/has_procedure_usage.rb
@@ -1,12 +1,22 @@
+require_relative '../with_tempfile'
+
 module Gobstones::Expectations
   class HasProcedureUsage
+    include Gobstones::WithTempfile
+
     def initialize(source_code, procedure_name)
       @source_code = source_code
       @procedure_name = procedure_name
     end
 
     def value
-      @procedure_name == 'IrAlBorde'
+      get_ast.match /AST\(procCall\s*#{@procedure_name}/
+    end
+
+    private
+
+    def get_ast
+      %x"gbs #{write_tempfile(@source_code, 'gbs').path} --print-ast --no-print-board --no-print-retvals"
     end
   end
 end
