f5a5be08016fc4c692d365db60e2d368765f9344
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-01 15:23:57 -0300

    Removing unnecesary program language config

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -3,16 +3,7 @@ require 'yaml'
 require 'gobstones'
 
 class TestRunner < Mumukit::Stub
-  def program_language
-    case @config['program_language']
-      when 'gobstones' then Gobstones::Spec::Language::Gobstones
-      when 'ruby' then Gobstones::Spec::Language::Ruby
-      else
-        raise "Unknown language #{@config['program_language']}"
-    end
-  end
-
   def run_compilation!(test_definition)
-    Gobstones::Spec::Runner.new(program_language).run!(test_definition)
+    Gobstones::Spec::Runner.new(Gobstones::Spec::Language::Gobstones).run!(test_definition)
   end
 end
