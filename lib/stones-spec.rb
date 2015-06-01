64eb4b0a9c6e10c8f525b92de4d1ce1aa9c9baeb
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-01 16:47:18 -0300

    Fixing require's order

--- lib/stones-spec.rb
+++ lib/stones-spec.rb
@@ -3,11 +3,12 @@ end
 
 require 'stones'
 
+require_relative 'stones-spec/with_tempfile'
+require_relative 'stones-spec/with_command_line'
+require_relative 'stones-spec/version'
+
 require_relative 'stones-spec/language'
 require_relative 'stones-spec/error_message_parser'
 require_relative 'stones-spec/example'
 require_relative 'stones-spec/runner'
 
-require_relative 'stones-spec/with_tempfile'
-require_relative 'stones-spec/with_command_line'
-require_relative 'stones-spec/version'
