fe16ee9c303b9f4af817f5e1d08c69d192e16066
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-02 18:01:49 -0300

    Moving MessageParser to gobstones module

--- lib/stones-spec.rb
+++ lib/stones-spec.rb
@@ -8,7 +8,6 @@ require_relative 'stones-spec/with_command_line'
 require_relative 'stones-spec/version'
 
 require_relative 'stones-spec/language'
-require_relative 'stones-spec/error_message_parser'
 require_relative 'stones-spec/example'
 require_relative 'stones-spec/runner'
 
