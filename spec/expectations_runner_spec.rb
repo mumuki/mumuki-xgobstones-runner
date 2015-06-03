a276853eba4fe24a70018edf9712de71c34b9588
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-03 01:25:05 -0300

    Adding test that reproduces the issue

--- spec/expectations_runner_spec.rb
+++ spec/expectations_runner_spec.rb
@@ -6,10 +6,21 @@ require_relative '../lib/expectations_runner'
 describe ExpectationsRunner do
   let(:runner) { ExpectationsRunner.new }
   let(:program) { 'program { Foo() } procedure Foo() {}' }
+  let(:bar) {   'program {
+    DibujarMacetero(Rojo)
+    IrAlBorde(Este) ; Mover(Oeste)
+    DibujarMacetero(Azul)
+    IrAlBorde(Norte) ; Mover(Sur)
+    DibujarMacetero(Verde)
+    IrAlBorde(Oeste)
+    DibujarMacetero(Negro)
+}' }
 
   let(:foo_expectation) { {'binding' => 'program', 'inspection' => 'HasUsage:Foo'} }
   let(:bar_expectation) { {'binding' => 'program', 'inspection' => 'HasUsage:Bar'} }
   let(:unknown_expectation) { {'binding' => 'program', 'inspection' => 'HasBinding'} }
+  let(:foo) { {'binding' => 'program', 'inspection' => 'HasUsage:DibujarMacetero' }  }
+
 
   it { expect(runner.run_expectations!(
                   [foo_expectation],
@@ -22,5 +33,10 @@ describe ExpectationsRunner do
   it { expect(runner.run_expectations!(
                   [unknown_expectation],
                   program)).to eq [{'expectation' => unknown_expectation, 'result' => false}] }
+
+  it { expect(runner.run_expectations!(
+                  [foo],
+                  bar)).to eq [{'expectation' => foo, 'result' => true}] }
+
 
 end
