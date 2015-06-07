87bcbd22249cc4b81b7c14646ce2e02e9fb6b5f6
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-07 14:57:08 -0300

    Inlining some methods

--- lib/stones-spec/runner.rb
+++ lib/stones-spec/runner.rb
@@ -10,9 +10,11 @@ module StonesSpec
 
     def run!(test_definition)
       subject = Subject.from(test_definition[:subject])
+      source = test_definition[:source]
+      check_head_position = test_definition[:check_head_position]
+
       results = test_definition[:examples].map do |example_definition|
-        example_definition[:check_head_position] = test_definition[:check_head_position]
-        run_example!(example_definition, test_definition[:source], subject)
+        run_example!(example_definition, check_head_position, source, subject)
       end
       aggregate_results(results)
     end
@@ -23,28 +25,27 @@ module StonesSpec
       language.test_program(source, subject)
     end
 
-    def run_example!(example_definition, source, subject)
-      result, status = start_example(source, example_definition, subject)
-      post_process result, status
+    def run_example!(example_definition, check_head_position, source, subject)
+      example = StonesSpec::Example.new(check_head_position, language, subject)
+      result, status = example.start!(
+          source,
+          example_definition[:initial_board],
+          example_definition[:final_board],
+          example_definition[:arguments])
+
+      if status == :passed
+        example.result
+      else
+        [example.parse_error_message(result), status]
+      end
+    ensure
+      example.stop!
     end
 
     def aggregate_results(results)
       [results.map { |it| it[0] }.join("\n<hr>\n"), results.all? { |it| it[1] == :passed } ? :passed : :failed]
     end
 
-    def post_process(result, status)
-      if status == :passed
-        @example.result
-      else
-        [@example.parse_error_message(result), status]
-      end
-    ensure
-      @example.stop!
-    end
 
-    def start_example(source, example_definition, subject)
-      @example = StonesSpec::Example.new(example_definition[:check_head_position], language, subject)
-      @example.start!(source, example_definition[:initial_board], example_definition[:final_board], example_definition[:arguments])
-    end
   end
 end
