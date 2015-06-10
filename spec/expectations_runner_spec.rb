b7184eb8fe78dc881e4f7d9bb74b401a7f688c79
Federico Aloi <federico.aloi@gmail.com>
2015-06-09 22:03:32 -0300

    Organised expectations tests

--- spec/expectations_runner_spec.rb
+++ spec/expectations_runner_spec.rb
@@ -5,23 +5,30 @@ require_relative '../lib/expectations_runner'
 
 describe ExpectationsRunner do
   let(:runner) { ExpectationsRunner.new }
-  let(:program) { 'program { Foo() } procedure Foo() {}' }
 
-  let(:foo_expectation) { {'binding' => 'program', 'inspection' => 'HasUsage:Foo'} }
-  let(:bar_expectation) { {'binding' => 'program', 'inspection' => 'HasUsage:Bar'} }
-  let(:unknown_expectation) { {'binding' => 'program', 'inspection' => 'HasBinding'} }
+  context 'Unknown expectation' do
+    let(:program) { 'program { Foo() } procedure Foo() {}' }
+    let(:unknown_expectation) { {'binding' => 'program', 'inspection' => 'HasBinding'} }
 
-  it { expect(runner.run_expectations!(
-                  [foo_expectation],
-                  program)).to eq [{'expectation' => foo_expectation, 'result' => true}] }
-
-  it { expect(runner.run_expectations!(
-                  [bar_expectation],
-                  program)).to eq [{'expectation' => bar_expectation, 'result' => false}] }
-
-  it { expect(runner.run_expectations!(
+    it { expect(runner.run_expectations!(
                   [unknown_expectation],
                   program)).to eq [{'expectation' => unknown_expectation, 'result' => false}] }
+  end
+
+  context 'HasUsage' do
+    let(:program) { 'program { Foo() } procedure Foo() {}' }
+
+    let(:foo_expectation) { {'binding' => 'program', 'inspection' => 'HasUsage:Foo'} }
+    let(:bar_expectation) { {'binding' => 'program', 'inspection' => 'HasUsage:Bar'} }
+
+    it { expect(runner.run_expectations!(
+                    [foo_expectation],
+                    program)).to eq [{'expectation' => foo_expectation, 'result' => true}] }
+
+    it { expect(runner.run_expectations!(
+                    [bar_expectation],
+                    program)).to eq [{'expectation' => bar_expectation, 'result' => false}] }
+  end
 
   context 'when procedure definitions are missing' do
     let(:program_with_extra_code) { 'procedure DibujarJardin() { DibujarMacetero(Rojo) }' }
