476845232186e8a2976b76c68c2f5531ed35f39c
Federico Aloi <federico.aloi@gmail.com>
2015-06-12 17:36:33 -0300

    I don't even need the OpenStruct

--- lib/test_compiler.rb
+++ lib/test_compiler.rb
@@ -1,6 +1,5 @@
 require 'mumukit'
 require 'yaml'
-require 'ostruct'
 require 'active_support/core_ext/hash'
 
 require_relative 'with_source_concatenation'
@@ -9,9 +8,9 @@ class TestCompiler < Mumukit::Stub
   include WithSourceConcatenation
 
   def create_compilation!(test_src, extra_src, content_src)
-    test = OpenStruct.new (YAML::load test_src)
-    test.source = concatenate_source(content_src, extra_src)
-    test.check_head_position = !!test.check_head_position
-    test.to_h.deep_symbolize_keys
+    test = YAML::load test_src
+    test['source'] = concatenate_source(content_src, extra_src)
+    test['check_head_position'] = !!test['check_head_position']
+    test.deep_symbolize_keys
   end
 end
