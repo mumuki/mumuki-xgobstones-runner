387720929df876ab1aa390ce0858e2ed72edec4c
Federico Aloi <federico.aloi@gmail.com>
2015-06-12 13:06:15 -0300

    Renamed add_caption -> with_caption

--- lib/stones-spec/with_gbb_html_rendering.rb
+++ lib/stones-spec/with_gbb_html_rendering.rb
@@ -7,14 +7,14 @@ module StonesSpec
 
       %x"#{Language::Gobstones.run(identity, board, board_html)}"
 
-      add_caption caption, board_html.read
+      with_caption caption, board_html.read
     ensure
       [identity, board, board_html].compact.each(&:unlink)
     end
 
     private
 
-    def add_caption(caption, board_html)
+    def with_caption(caption, board_html)
       board_html.sub '<table class="gbs_board">', "<table class=\"gbs_board\">\n<caption>#{caption}</caption>"
     end
   end
