2088e99f54d27cab9b7631a239674affa1c2cad4
Federico Aloi <federico.aloi@gmail.com>
2015-06-14 18:04:56 -0300

    Added status as a describe scoped let

--- spec/gobstones_runner_spec.rb
+++ spec/gobstones_runner_spec.rb
@@ -12,44 +12,40 @@ describe Runner do
     let(:lang) { Language::Gobstones }
     let(:runner) { Runner.new(lang) }
     let(:html) { results[0] }
+    let(:status) { results[1] }
 
     describe 'procedure spec' do
       context 'when passes' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/gobstones/procedure/move_to_origin_ok.yml') }
-
-        it { expect(results[1]).to eq :passed }
+        it { expect(status).to eq :passed }
       end
 
       context 'when passes with arguments' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/gobstones/procedure/times_move_ok.yml') }
-
-        it { expect(results[1]).to eq :passed }
+        it { expect(status).to eq :passed }
       end
 
       context 'when fails' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/gobstones/procedure/move_to_origin_fail.yml') }
-
-        it { expect(results[1]).to eq :failed }
+        it { expect(status).to eq :failed }
       end
 
       context 'when fails with arguments' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/gobstones/procedure/times_move_fail.yml') }
-
-        it { expect(results[1]).to eq :failed }
+        it { expect(status).to eq :failed }
       end
     end
 
     describe 'function spec' do
       context 'when passes with args' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/gobstones/function/remaining_cells_ok.yml') }
-
-        it { expect(results[1]).to eq :passed }
+        it { expect(status).to eq :passed }
       end
 
       context 'when fails with args' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/gobstones/function/remaining_cells_fail.yml') }
 
-        it { expect(results[1]).to eq :failed }
+        it { expect(status).to eq :failed }
         it { expect(html).to eq 'Se esperaba <b>9</b> pero se obtuvo <b>18</b>' }
       end
     end
@@ -58,18 +54,18 @@ describe Runner do
       context 'can check head position' do
         context 'when its wrong' do
           let(:results) { runner.run!(YAML.load_file 'spec/data/head_position_wrong.yml') }
-          it { expect(results[1]).to eq(:failed) }
+          it { expect(status).to eq(:failed) }
         end
 
         context 'when its ok' do
           let(:results) { runner.run!(YAML.load_file 'spec/data/head_position_ok.yml') }
-          it { expect(results[1]).to eq(:passed) }
+          it { expect(status).to eq(:passed) }
         end
       end
 
       context 'doesnt check head position if the flag is false' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/dont_check_head_position.yml') }
-        it { expect(results[1]).to eq(:passed) }
+        it { expect(status).to eq(:passed) }
       end
 
       context 'doesnt include the initial board if the flag is false' do
@@ -81,7 +77,7 @@ describe Runner do
         context 'when the final board matches' do
           let(:results) { runner.run!(YAML.load_file 'spec/data/red_ball_at_origin.yml') }
 
-          it { expect(results[1]).to eq(:passed) }
+          it { expect(status).to eq(:passed) }
 
           context 'should return an html representation of the initial and final board as result' do
             it { expect(html).to include(File.new('spec/data/red_ball_at_origin_initial.html').read) }
@@ -93,7 +89,7 @@ describe Runner do
 
         context 'when the final board doesnt match' do
           let(:results) { runner.run!(YAML.load_file 'spec/data/red_ball_at_origin_wrong.yml') }
-          it { expect(results[1]).to eq(:failed) }
+          it { expect(status).to eq(:failed) }
 
           context 'should return an html representation of the initial, expected and actual boards as result' do
             it { expect(html).to include(File.new('spec/data/red_ball_at_origin_initial.html').read) }
@@ -106,7 +102,8 @@ describe Runner do
 
         context 'when produces BOOM' do
           let(:results) { runner.run!(YAML.load_file 'spec/data/runtime_error.yml') }
-          it { expect(results[1]).to eq(:failed) }
+
+          it { expect(status).to eq(:failed) }
 
           it { expect(html).to include(File.new('spec/data/runtime_error_initial.html').read) }
           it do
@@ -128,7 +125,8 @@ Error en tiempo de ejecución:
 
       context 'when the file is not sintactically ok,' do
         let(:results) { runner.run!(YAML.load_file 'spec/data/syntax_error.yml') }
-        it { expect(results[1]).to eq(:failed) }
+
+        it { expect(status).to eq(:failed) }
         it do
           expect(html).to eq(
 '<pre>cerca de un identificador con mayúscula "Error"
