8b39bdabec735178c9577ef658df942f17b7a23f
Federico Aloi <federico.aloi@gmail.com>
2015-06-15 19:39:33 -0300

    The gobstones output doesn't include the traceback anymore

--- lib/stones-spec/language/gobstones.rb
+++ lib/stones-spec/language/gobstones.rb
@@ -60,10 +60,6 @@ module StonesSpec::Language
   end
 
   class ErrorMessageParser
-    def remove_traceback (x)
-      x.take_while { |str| not str.start_with? 'Traceback' }
-    end
-
     def remove_line_specification(x)
       x.drop(3)
     end
@@ -73,7 +69,7 @@ module StonesSpec::Language
     end
 
     def parse(result)
-      remove_boom_line_specification(remove_traceback(remove_line_specification(result.lines))).join.strip
+      remove_boom_line_specification(remove_line_specification(result.lines)).join.strip
     end
   end
 end
