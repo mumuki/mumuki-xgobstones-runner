791774a7952cd0cade1c84f10f156a5b6bdedcc3
gcrespi <crespi.gustavo@gmail.com>
2015-09-05 18:06:07 -0300

    Takes path from Sinatra's config and pases it to the Runner in stones-spec

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -3,7 +3,11 @@ require 'yaml'
 require 'stones-spec'
 
 class TestRunner < Mumukit::Stub
+  def gobstones_command
+    @config['gobstones_command']
+  end
+
   def run_compilation!(test_definition)
-    StonesSpec::Runner.new(StonesSpec::Language::Gobstones).run!(test_definition)
+    StonesSpec::Runner.new(StonesSpec::Language::Gobstones, gobstones_command).run!(test_definition)
   end
 end
